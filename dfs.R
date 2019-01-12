6171e41acc60318f16cea00ccf529e5d18ede29c
David-PC <dk9946@student.uni-lj.si>
2019-01-12 09:56:07 +0100

    created dfs

---
+++ dfs.R
@@ -0,0 +1,56 @@
+depth.first <- function(graph, startNode, endNodes)
+{
+  if (is.null(rownames(graph)))
+    vNames <- 1:nrow(graph)
+  else
+    vNames <- rownames(graph)
+  
+  startNode <- which(vNames %in% startNode)
+  endNodes <- which(vNames %in% endNodes)
+  
+  
+  stack <- vector()
+  marked <- rep(FALSE, len = nrow(graph))
+  from <- rep(-1, len = nrow(graph))
+  
+  marked[startNode] <- TRUE	
+  stack <- c(stack, startNode)
+  print(paste("Polagam na sklad vozlisce", vNames[startNode]))
+  
+  
+  while (length(stack) > 0)
+  {
+    curNode <- stack[length(stack)]
+    
+    if (curNode %in% endNodes)
+    {
+      print(paste("Resitev DFS v vozliscu", vNames[curNode]))
+      
+      path <- vNames[curNode]
+      while (TRUE)
+      {
+        curNode <- from[curNode]
+        if (curNode != -1)
+          path <- paste(path, "<--", vNames[curNode])
+        else
+          return(path)
+      }
+    }
+    
+    sel <- which(!is.na(graph[curNode,]) & !marked)
+    if (any(sel))
+    {
+      nextNode <- sel[1]
+      marked[nextNode] <- TRUE
+      from[nextNode] <- curNode
+      stack <- c(stack, nextNode)
+      
+      print(paste("Polagam na sklad vozlisce", vNames[nextNode]))
+    }
+    else
+    {
+      stack <- stack[-length(stack)]
+      print(paste("Odstranjujem s sklada vozlisce", vNames[curNode]))
+    }
+  }
+}
