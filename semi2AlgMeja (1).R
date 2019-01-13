f6bc470b93efdc1da6e09d97ba3099edf3ee540f
David-PC <dk9946@student.uni-lj.si>
2019-01-13 14:25:25 +0100

    aljaževo

--- semi2AlgMeja (1).R
+++ semi2AlgMeja (1).R
@@ -33,8 +33,8 @@ plotLabyrinth <- function(lab)
 }
 
 
-matrikaSosedov <- function(data){
-	Sys.sleep(0.1)
+mainMeja <- function(data,myLvl,maxLvl){
+	if (myLvl >= maxLvl) return(FALSE)
 	currentPosition=which(data == -2,TRUE)
 	data[currentPosition[1],currentPosition[2]]=-4	
 
@@ -43,54 +43,101 @@ matrikaSosedov <- function(data){
 	gor=data[currentPosition[1]-1,currentPosition[2]]
 	levo=data[currentPosition[1],currentPosition[2]-1]
 	
+	x=c(dol,desno,gor,levo)
+	if (any(x==-3)){
+		data[currentPosition[1]+1,currentPosition[2]] = -4
+		screen <- plotLabyrinth(data)
+		return(data)
+	}
+
 	pathFound=FALSE
-	if (dol == -3) return(TRUE)
-	if (!pathFound && dol > 0){
+	if (dol >= 0){
 		tempInt=data[currentPosition[1]+1,currentPosition[2]]
 		data[currentPosition[1]+1,currentPosition[2]] = -2
 		screen <- plotLabyrinth(data)
-		pathFound=matrikaSosedov(data)
+		pathFound=mainMeja(data,myLvl+1,maxLvl)
 		data[currentPosition[1]+1,currentPosition[2]] = tempInt
 	}
-	if (desno == -3) return(TRUE)
-	if (!pathFound && desno > 0){
+	if (!is.matrix(pathFound) && desno >= 0){
 		tempInt=data[currentPosition[1],currentPosition[2]+1]
 		data[currentPosition[1],currentPosition[2]+1] = -2
 		screen <- plotLabyrinth(data)
-		pathFound=matrikaSosedov(data)
+		pathFound=mainMeja(data,myLvl+1,maxLvl)
 		data[currentPosition[1],currentPosition[2]+1] = tempInt
 	}
-	if (gor == -3) return(TRUE)
-	if (!pathFound && gor > 0){
+	if (!is.matrix(pathFound) && gor >= 0){
 		tempInt=data[currentPosition[1]-1,currentPosition[2]]
 		data[currentPosition[1]-1,currentPosition[2]] = -2
 		screen <- plotLabyrinth(data)
-		pathFound=matrikaSosedov(data)
+		pathFound=mainMeja(data,myLvl+1,maxLvl)
 		data[currentPosition[1]-1,currentPosition[2]] = tempInt
 	}
-	if (levo == -3) return(TRUE)
-	if (!pathFound && levo > 0){
+	if (!is.matrix(pathFound) && levo >= 0){
 		tempInt=data[currentPosition[1],currentPosition[2]-1]
 		data[currentPosition[1],currentPosition[2]-1] = -2
 		screen <- plotLabyrinth(data)
-		pathFound=matrikaSosedov(data)
+		pathFound=mainMeja(data,myLvl+1,maxLvl)
 		data[currentPosition[1],currentPosition[2]-1] = tempInt
 	}
 	return(pathFound)
 }
 
+meja <- function(data){
+	i=1
+	rez=FALSE
+	while (!is.matrix(rez)){
+		rez=mainMeja(data,0,i)
+		i=i+6
+	}
+	return(rez)
+}
 
-data <- read.table("labyrinth_1.txt", sep=",", header=F)
+getPath<- function(data,cp){
+	pathData=cp
+	dol=data[cp[1]+1,cp[2]]
+	desno=data[cp[1],cp[2]+1]
+	gor=data[cp[1]-1,cp[2]]
+	levo=data[cp[1],cp[2]-1]
+
+	x=c(dol,desno,gor,levo)
+	
+	if(dol == -4)
+		cp[1]=cp[1]+1
+	else if(desno == -4)
+		cp[2]=cp[2]+1
+	else if(gor == -4)
+		cp[1]=cp[1]-1
+	else if(levo == -4)
+		cp[2]=cp[2]-1
+	if (!any(x==-4)){
+		data[pathData[1],pathData[2]]=-5
+		if(dol == -3)
+			cp[1]=cp[1]+1
+		else if(desno == -3)
+			cp[2]=cp[2]+1
+		else if(gor == -3)
+			cp[1]=cp[1]-1
+		else if(levo == -3)
+			cp[2]=cp[2]-1
+		return(rbind(pathData,cp))
+	}
+	
+	data[pathData[1],pathData[2]]=-5
+	return(rbind(pathData,getPath(data,cp)))
+}
+
+getPoints<- function(data,matrix){
+	points=0
+	for(i in 1:nrow(matrix)){
+		points=points+data[matrix[i,1],matrix[i,2]]	
+	}
+	return(points)
+}
+
+data <- read.table("labyrinth_11.txt", sep=",", header=F)
 data <- as.matrix(data)
 screen <- plotLabyrinth(data)
 
-matrikaSosedov(data)
-
-
-
-
-
-
-
-
-
+mx=meja(data)
+a=getPath(mx,which(data == -2, TRUE))
+getPoints(data,a)
